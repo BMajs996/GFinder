f90b31ebf9a20bd78a200b009709893f7ac38d0c
Bizgis <bojan.majstorovic@gmail.com>
2020-02-20 13:35:42 +0100

    Update github.js

--- github.js
+++ github.js
@@ -1,7 +1,7 @@
 class Github {
   constructor() {
-    this.client_id = 'd9308aacf8b204d361fd';
-    this.client_secret = '84969aeef73956f4ec9e8716d1840532802bb81b';
+    this.client_id = '1ff160436323964a750c';
+    this.client_secret = 'e0fe528be60e49c1935ccfa0f1547fe3d1640fbe';
     this.repos_count = 5;
     this.repos_sort = 'created: asc';
   }
